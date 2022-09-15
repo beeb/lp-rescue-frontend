7b801b3ab3f8b6b100e5ab9ecbfccde1f3f539e7
beeb <vbersier@gmail.com>
2022-09-15 21:33:27 +0200

    Added ethers and web3 modal

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
-	import '../app.css';
+	import '../app.css'
 </script>
 
-<slot />
+<div class="max-w-screen-xl mx-auto my-6">
+	<slot />
+</div>
 
 <svelte:head>
 	<title>LP+Rescue - add liquidity to stuck pairs</title>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,56 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { onMount } from 'svelte'
+	import { ethers } from 'ethers'
+	import { signerAddress, defaultEvmStores } from 'svelte-ethers-store'
+	import Web3Modal from 'web3modal'
+	import WalletConnectProvider from '@walletconnect/web3-provider/dist/umd/index.min.js'
+
+	let web3Modal: Web3Modal | undefined
+
+	// TODO: get URL from network select dropdown
+	const defaultProvider = new ethers.providers.JsonRpcProvider('https://bsc-dataseed.binance.org')
+
+	const connect = async () => {
+		const modalProvider = await web3Modal?.connect()
+		await defaultEvmStores.setProvider(modalProvider)
+	}
+
+	const disconnect = async () => {
+		if (web3Modal?.cachedProvider) {
+			web3Modal?.clearCachedProvider()
+		}
+		await defaultEvmStores.disconnect()
+		await defaultEvmStores.setProvider(defaultProvider)
+	}
+
+	onMount(async () => {
+		web3Modal = new Web3Modal({
+			cacheProvider: true,
+			theme: 'dark',
+			providerOptions: {
+				walletconnect: {
+					package: WalletConnectProvider,
+					options: {
+						rpc: {
+							56: 'https://bsc-dataseed.binance.org/'
+						}
+					}
+				}
+			}
+		})
+
+		if (web3Modal.cachedProvider) {
+			await connect()
+		}
+	})
+</script>
+
+<h1 class="text-3xl mb-3">Welcome to Web3</h1>
+<p class="mb-3">{$signerAddress}</p>
+<p>
+	{#if $signerAddress}
+		<button type="button" class="btn" on:click={() => disconnect()}>Disconnect</button>
+	{:else}
+		<button type="button" class="btn" on:click={() => connect()}>Connect</button>
+	{/if}
+</p>

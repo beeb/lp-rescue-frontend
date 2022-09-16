1d2b967d69a33bd91f2c16527b698b79dbec052d
beeb <vbersier@gmail.com>
2022-09-16 18:45:09 +0200

    Added call to action

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-	import { signerAddress, chainId } from 'svelte-ethers-store'
 	import TitleBar from '$lib/components/TitleBar.svelte'
+	import CrossIcon from 'virtual:icons/ri/add-circle-line'
 </script>
 
 <TitleBar />
@@ -12,9 +12,9 @@
 	<div class="card w-10/12 max-w-xl bg-base-200 shadow-lg">
 		<div class="card-body">
 			<h2 class="card-title text-3xl mb-4">Card title!</h2>
-			<p>If a dog chews shoes whose shoes does he choose?</p>
+			<p class="mb-4">If a dog chews shoes whose shoes does he choose?</p>
 			<div class="card-actions justify-end">
-				<button class="btn btn-lg btn-primary">Action</button>
+				<button class="btn btn-lg btn-primary gap-2"><CrossIcon /> Rescue LP</button>
 			</div>
 		</div>
 	</div>

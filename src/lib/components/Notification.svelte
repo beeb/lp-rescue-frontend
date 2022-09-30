bf8031a82b5e52ad0ee248159f51cf9d5a6c393d
beeb <vbersier@gmail.com>
2022-09-30 21:55:17 +0200

    feat: add toast notifications

---
+++ src/lib/components/Notification.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	import type { DefaultNotificationOptions } from 'svelte-notifications'
+
+	export let notification: DefaultNotificationOptions = {
+		text: '',
+		position: 'bottom-left'
+	}
+
+	export let onRemove: Function | null = null
+</script>
+
+<div
+	class="toast toast-start toast-bottom toast-"
+	class:toast-start={notification.position === 'top-left' || notification.position === 'bottom-left'}
+	class:toast-center={notification.position === 'top-center' || notification.position === 'bottom-center'}
+	class:toast-end={notification.position === 'top-right' || notification.position == 'bottom-right'}
+	class:toast-top={notification.position === 'top-left' || notification.position === 'top-right'}
+	class:toast-bottom={notification.position === 'bottom-left' || notification.position == 'bottom-right'}
+>
+	<div
+		class="alert"
+		class:alert-error={notification.type === 'error'}
+		class:alert-success={notification.type === 'success'}
+		class:alert-warning={notification.type === 'warning'}
+		class:alert-info={notification.type === 'info'}
+	>
+		<div>
+			<span>{@html notification.text}</span>
+		</div>
+		<div class="flex-none">
+			<button
+				type="button"
+				class="btn btn-sm btn-square btn-ghost"
+				on:click={() => {
+					onRemove && onRemove()
+				}}>✕</button
+			>
+		</div>
+	</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,35 +8,39 @@
 	import Step3 from '$lib/components/Step3.svelte'
 	import { step } from '$lib/stores/app'
 	import Github from '$lib/assets/github.svg?component'
+	import Notification from '$lib/components/Notification.svelte'
+	import Notifications from 'svelte-notifications'
 </script>
 
-<div class="relative">
-	<div class="flex flex-col w-full min-h-screen justify-between items-center gap-10 pb-12">
-		<div class="w-full">
-			<TitleBar />
-		</div>
-		<div class="flex-grow w-full flex flex-col justify-center items-center gap-10">
-			<div>
-				<Steps step={$step} />
+<Notifications item={Notification}>
+	<div class="relative">
+		<div class="flex flex-col w-full min-h-screen justify-between items-center gap-10 pb-12">
+			<div class="w-full">
+				<TitleBar />
 			</div>
+			<div class="flex-grow w-full flex flex-col justify-center items-center gap-10">
+				<div>
+					<Steps step={$step} />
+				</div>
 
-			<div class="grid grid-cols-1 w-10/12 max-w-2xl">
-				{#if $step == 0}
-					<Step0 />
-				{:else if $step == 1}
-					<Step1 />
-				{:else if $step == 2}
-					<Step2 />
-				{:else if $step == 3}
-					<Step3 />
-				{:else}
-					<div in:fly={{ x: 200 }} out:fly={{ x: -200 }}>Not sure how you got here...</div>
-				{/if}
+				<div class="grid grid-cols-1 w-10/12 max-w-2xl">
+					{#if $step == 0}
+						<Step0 />
+					{:else if $step == 1}
+						<Step1 />
+					{:else if $step == 2}
+						<Step2 />
+					{:else if $step == 3}
+						<Step3 />
+					{:else}
+						<div in:fly={{ x: 200 }} out:fly={{ x: -200 }}>Not sure how you got here...</div>
+					{/if}
+				</div>
 			</div>
+		</div>
+
+		<div class="absolute right-4 bottom-4">
+			<a href="https://github.com/beeb/lp-rescue-frontend"><Github width="3rem" /></a>
 		</div>
 	</div>
-
-	<div class="absolute right-4 bottom-4">
-		<a href="https://github.com/beeb/lp-rescue-frontend"><Github width="3rem" /></a>
-	</div>
-</div>
+</Notifications>
